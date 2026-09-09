451b44042feb7815006356ee6eaa43601bff7474
sunzid874-code <sunzid874@gmail.com>
2026-09-10 03:58:58 +0600

    crete app.jsx

---
+++ app.jsx
@@ -0,0 +1,11 @@
+import StudentLoginPage from './pages/StudentLoginPage'; // আপনার ফাইলের লোকেশন ও নাম অনুযায়ী দিন
+
+function App() {
+  return (
+    <div>
+      <StudentLoginPage />
+    </div>
+  );
+}
+
+export default App;
